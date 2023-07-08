dccf62ff077bc469bab90d862440e63ef3e81b6b
00codester <00codester@gmail.com>
2023-07-08 16:01:56 -0600

    remove api key

--- js/api.js
+++ js/api.js
@@ -7,13 +7,13 @@ async function apiFetch(url){
     try{
         const response = await fetch(url);
         if(response.ok){
-            console.log(response);
+            //console.log(response);
         } else {
             throw Error(await response.text());
         }
        
     } catch (error){
-        console.log(error);
+        //console.log(error);
     }
 
 }
